48eb56891680bd88df218c9e1e718ab3f8c7b8e0
Ryan Zayne <phoenixastro97@gmail.com>
2024-01-17 12:59:52 +0100

    setup

---
+++ test-1.js
@@ -0,0 +1,31 @@
+// const myName = 'Zayne';
+// const today = 'Wednesday';
+// const greeting = 'Hello';
+
+// function sum(a, b) {
+// 	return a + b;
+// }
+
+// module.exports = myName;
+
+
+
+
+
+
+
+
+
+
+
+let name = 'Zayne';
+let age = 20;
+let country = 'Nigeria';
+
+
+
+
+module.exports.name = name;
+module.exports.age = age;
+module.exports.country = country;
+module.exports.sum = sum;
