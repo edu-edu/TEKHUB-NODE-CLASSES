34c5cd8b59721bbf2cbd0db80dba2d9d61754cdf
edu-edu <115025570+edu-edu@users.noreply.github.com>
2024-01-24 23:43:30 +0100

    Add files via upload

---
+++ assignment/task.js
@@ -0,0 +1,19 @@
+const Ada = {
+age: 19,
+married: "yes",
+}
+Ada['surname'] = 'Mgbeke'
+console.log(Ada)
+
+const nameMeaning = {
+    Anwuli: 'Joy',
+    Edu: 'Guide',
+    Ututu: 'Morning',
+    Ego: 'Money'
+}
+
+delete nameMeaning.Anwuli
+delete nameMeaning.Edu
+delete nameMeaning.Ututu
+
+console.log(nameMeaning)
